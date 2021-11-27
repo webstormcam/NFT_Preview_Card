c577fc903fcfeceb622ec903a848c95f68b6aa3f
Cameron Chandler <webstormcam@gmail.com>
2021-11-26 23:45:35 -0500

    Lol this silly js problem lol

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let layer=document.getElementById('layer')
+let sizeImg = document.getElementById('sizeImg')
 let eye = document.getElementById('eye')
+
+sizeImg.addEventListener('mouseover',function(event){
+    
+   if(eye.style.display ==='none'){
+       eye.style.display ='block'
+   } else{
+       eye.style.display ='none'
+   }
+   console.log('hello')
+})
